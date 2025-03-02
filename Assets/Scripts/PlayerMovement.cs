14a393eb3ec24ed3d7b02a73cd9f386dd01e98a6
09ejacob <elias.garseth.jacobsen@gmail.com>
2025-03-02 21:52:50 +0100

    Continued with adding lobby scene and making it possible to create and join rooms. Also continued on loading scene. Also started on making it possible to individually move player instances but that does not work yet.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour {
@@ -21,6 +22,8 @@ public class PlayerMovement : MonoBehaviour {
     private CharacterController characterController;
 
     private bool canMove = true;
+
+    PhotonView view;
 
     void Start() {
         characterController = GetComponent<CharacterController>();
